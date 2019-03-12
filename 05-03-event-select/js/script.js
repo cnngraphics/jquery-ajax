3c5762f26091d8990a10c11fa21867fe6e1b9d4e
Theo Marcellus <cnngraphics@gmail.com>
2019-03-11 20:09:09 -0400

    Updating result conataine with selection from dropdown

--- 05-03-event-select/js/script.js
+++ 05-03-event-select/js/script.js
@@ -2,9 +2,16 @@
 $(document).ready(function() {
 	$('#example').on('click', 'button', function(){ 	//get the list and delegate the click to the btuuton
 		var selected = $('#place option:selected'); // find selected item by list having option that is selected
+		var value = selected.val();
+		var price = selected.data('price')
+		console.log(value);
 		console.log(selected.data('price')) //find the value that is selected by it's JQ dat('price')
 		selected.attr('data-price'); // standard js aquivalent to the aobe JQ method
 		// console.log(selected);
+		if(price){
+			$('#result').html(value+' '+price)
+		}
+		
 	});
 
 });
