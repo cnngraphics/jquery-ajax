298493f47865d33a0693b49abb834ed174f27beb
Theo Marcellus <cnngraphics@gmail.com>
2019-03-12 23:25:51 -0400

    Added AJAX Call - woot woot!

--- 08-01-ajax/js/script.js
+++ 08-01-ajax/js/script.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
-	$('#example').on('click', 'button.switch', function() {
-		$.ajax('result.html')
-			.done(function(response){
-				$('#result').html(response);
-			});		
-	});	
+$(document).ready(function(){
+	//selection and event delegation
+	$('#example').on('click', 'button.switch', function(){
+		$.ajax('page.html') //takes one or 2 args, 2nd arg is object taking methods
+		.done(function( response ){ //catch the result of the call into response
+			//select target to update
+			$('#result').html(response);
+		});
+	});
 });
